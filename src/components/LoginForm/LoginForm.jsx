b6a3ea726ee550ccdca4feac6048af487086d5b0
oxana <oxanamoisiuk7@gmail.com>
2025-05-10 22:03:27 +0300

    fix-forms

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,29 +1,56 @@
-import { Formik, Form, Field } from 'formik';
 import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 import { login } from '../../redux/auth/authOperations';
+import toast from 'react-hot-toast';
+import css from './LoginForm.module.css';
 
 const LoginForm = () => {
   const dispatch = useDispatch();
 
-  const handleSubmit = (values, { resetForm }) => {
-    dispatch(login(values));
-    resetForm();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    if (name === 'email') setEmail(value);
+    if (name === 'password') setPassword(value);
+  };
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    try {
+      await dispatch(login({ email, password })).unwrap();
+      toast.success('Вхід успішний!');
+    } catch (error) {
+      if (error === 'Request failed with status code 400') {
+        toast.error('Невірна пошта або пароль.');
+      } else {
+        toast.error('Помилка входу. Спробуйте пізніше.');
+      }
+    }
   };
 
   return (
-    <Formik initialValues={{ email: '', password: '' }} onSubmit={handleSubmit}>
-      <Form>
-        <label>
-          Email
-          <Field type="email" name="email" required />
-        </label>
-        <label>
-          Password
-          <Field type="password" name="password" required />
-        </label>
-        <button type="submit">Login</button>
-      </Form>
-    </Formik>
+    <form className={css.form} onSubmit={handleSubmit}>
+      <input
+        type="email"
+        name="email"
+        placeholder="Електронна пошта"
+        value={email}
+        onChange={handleChange}
+        required
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="Пароль"
+        value={password}
+        onChange={handleChange}
+        required
+      />
+      <button type="submit">Увійти</button>
+    </form>
   );
 };
 
--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,33 +1,66 @@
-import { Formik, Form, Field } from 'formik';
 import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 import { register } from '../../redux/auth/authOperations';
+import toast from 'react-hot-toast';
+import css from './RegistrationForm.module.css';
 
 const RegistrationForm = () => {
   const dispatch = useDispatch();
 
-  const handleSubmit = (values, { resetForm }) => {
-    dispatch(register(values));
-    resetForm();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    if (name === 'email') setEmail(value);
+    if (name === 'password') setPassword(value);
+    if (name === 'name') setName(value);
+  };
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    try {
+      await dispatch(register({ name, email, password })).unwrap();
+      toast.success('Реєстрація успішна!');
+    } catch (error) {
+      if (error === 'Request failed with status code 400') {
+        toast.error('Користувач із таким email уже існує.');
+      } else {
+        toast.error('Помилка при реєстрації. Спробуйте ще раз.');
+      }
+    }
   };
 
   return (
-    <Formik initialValues={{ name: '', email: '', password: '' }} onSubmit={handleSubmit}>
-      <Form>
-        <label>
-          Name
-          <Field type="text" name="name" required />
-        </label>
-        <label>
-          Email
-          <Field type="email" name="email" required />
-        </label>
-        <label>
-          Password
-          <Field type="password" name="password" required />
-        </label>
-        <button type="submit">Register</button>
-      </Form>
-    </Formik>
+    <form className={css.form} onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="name"
+        placeholder="Ваше ім’я"
+        value={name}
+        onChange={handleChange}
+        required
+      />
+      <input
+        type="email"
+        name="email"
+        placeholder="Електронна пошта"
+        value={email}
+        onChange={handleChange}
+        required
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="Пароль"
+        value={password}
+        onChange={handleChange}
+        required
+      />
+      <button type="submit">Зареєструватися</button>
+    </form>
   );
 };
 
